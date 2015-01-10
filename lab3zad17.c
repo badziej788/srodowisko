1cafd7025c0158348f1202e197140877264fbd59
badziej788 <badziej788@gmail.com>
2015-01-10 19:31:00 +0100

    Create lab3zad17.c

---
+++ lab3zad17.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int szescian();
+int main(int argc, char** argv)
+{
+	szescian();
+	return 0;
+}
+
+int szescian(){
+	int i = 1;
+	
+	while (i <= 10) {
+	
+		printf("%d\n",i*i*i);
+		i++;
+	}
+	printf("petla for :\n");
+	i = 1;
+	for (i == 1; i <= 10; i++) 
+	{
+	printf("%d \n", i*i*i);
+	}
+	
+}
